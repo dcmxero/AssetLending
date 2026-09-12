44ae4a3f737faac6c66e659475fec7705da8c746
Marek Marcinko <marek.marcinko@ngaviation.eu>
2026-09-12 16:44:19 +0200

    Allow the React dev server origin through CORS
    The React client serves on 4300 so both frontends can run side by side,
    but the default policy only listed the Angular origin.

--- Server/WebApi/Program.cs
+++ Server/WebApi/Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "http://localhost:4300")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
